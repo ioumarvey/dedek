bf9f7e820342dc91425efecf25bccfbdc125c4da
sal <96798879+ioumarvey@users.noreply.github.com>
2022-12-26 16:09:49 +0100

    added list options

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const DICE = 60;
 
+
   var form = document.getElementById("myForm");
+
 
   form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     var result = document.getElementById("roll").value;
+    var list = document.getElementById("lists").value;
 
-if (result > DICE || result <1) {
+
+if (result > DICE) {
         alert("cos ci sie pojebalo szefie");
       } else {
-        result %= 15;
-        if (result == 0) result = 15;
+        result %= list;
+        if (result == 0) result = list;
         alert(`Item, który otrzymujesz to ${result}.`);
       }
-});
+    }
+  );
